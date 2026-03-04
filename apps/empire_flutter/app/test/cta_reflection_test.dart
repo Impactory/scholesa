268b35129c66bcb0d29f2e821ab8674eed4e807e
UnifiedAI-ONeID <simon.luke@ufv.ca>
2026-03-03 19:51:41 -0800

    tweaks RC3

--- apps/empire_flutter/app/test/cta_reflection_test.dart
+++ apps/empire_flutter/app/test/cta_reflection_test.dart
@@ -1,12 +1,45 @@
+import 'package:fake_cloud_firestore/fake_cloud_firestore.dart';
+import 'package:firebase_auth/firebase_auth.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_test/flutter_test.dart';
+import 'package:mocktail/mocktail.dart';
+import 'package:nested/nested.dart';
+import 'package:provider/provider.dart';
 import 'package:scholesa_app/modules/educator/educator_mission_plans_page.dart';
+import 'package:scholesa_app/modules/educator/educator_service.dart';
 import 'package:scholesa_app/modules/site/site_sessions_page.dart';
+import 'package:scholesa_app/services/firestore_service.dart';
 
 final ThemeData _testTheme = ThemeData(
   useMaterial3: true,
   splashFactory: InkRipple.splashFactory,
 );
+
+class _MockFirebaseAuth extends Mock implements FirebaseAuth {}
+
+Future<void> _pumpMissionPlansPage(WidgetTester tester) async {
+  final FirestoreService firestoreService = FirestoreService(
+    firestore: FakeFirebaseFirestore(),
+    auth: _MockFirebaseAuth(),
+  );
+  final EducatorService educatorService = EducatorService(
+    firestoreService: firestoreService,
+    educatorId: 'educator-test-1',
+  );
+
+  await tester.pumpWidget(
+    MultiProvider(
+      providers: <SingleChildWidget>[
+        Provider<FirestoreService>.value(value: firestoreService),
+        ChangeNotifierProvider<EducatorService>.value(value: educatorService),
+      ],
+      child: MaterialApp(
+        theme: _testTheme,
+        home: const EducatorMissionPlansPage(),
+      ),
+    ),
+  );
+}
 
 void main() {
   group('CTA reflection regressions', () {
@@ -44,12 +77,8 @@ void main() {
         (WidgetTester tester) async {
       await tester.binding.setSurfaceSize(const Size(1200, 1600));
 
-      await tester.pumpWidget(
-        MaterialApp(
-          theme: _testTheme,
-          home: EducatorMissionPlansPage(),
-        ),
-      );
+      await _pumpMissionPlansPage(tester);
+      await tester.pumpAndSettle();
 
       await tester.tap(find.text('New Mission'));
       await tester.pumpAndSettle();
