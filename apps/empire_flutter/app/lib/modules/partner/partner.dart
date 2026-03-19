5af3dbdf1506b53b33b17e46e6de11916dc9c589
UnifiedAI-ONeID <simon.luke@ufv.ca>
2026-03-18 17:23:27 -0700

    latest fixes for gaps pre-rc3

--- apps/empire_flutter/app/lib/modules/partner/partner.dart
+++ apps/empire_flutter/app/lib/modules/partner/partner.dart
@@ -3,6 +3,8 @@ library;
 
 export 'partner_listings_page.dart';
 export 'partner_contracts_page.dart';
+export 'partner_deliverables_page.dart';
+export 'partner_integrations_page.dart';
 export 'partner_payouts_page.dart';
 export 'partner_models.dart';
 export 'partner_service.dart';
